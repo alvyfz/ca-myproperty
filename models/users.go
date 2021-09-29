c7432e665c8538dab2beb192b1e2c2dfd0185278
Alvy Fauzi <alvyfauzi10@gmail.com>
2021-09-29 20:24:26 +0700

    edit model user

--- models/users.go
+++ models/users.go
@@ -4,12 +4,10 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name        string    `json:"name"`
-	Email       string    `json:"email"`
-	Password    string    `json:"-"`
-	PhoneNumber string    `json:"phone_number"`
-	Gender      string    `json:"gender"`
-	City        string    `json:"city"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	PhoneNumber string `json:"phone_number"`
+	Gender      string `json:"gender"`
+	City        string `json:"city"`
 }
-
-
